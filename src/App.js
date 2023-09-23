6fa70dcae7a69a6b5cfbac9e90f644ac89b6c278
nishujangra <ndjangra1027@gmail.com>
2023-09-23 10:38:56 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Container, TextField, Button, Typography, Select, MenuItem } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import axios from 'axios';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
+  const [currencies, setCurrencies] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('INR');
+  const [amount, setAmount] = useState(1);
+  const [convertedAmount, setConvertedAmount] = useState(null);
+
+  useEffect(() => {
+
+    axios.get('https://api.exchangerate-api.com/v4/latest/USD').then((response) => {
+      setCurrencies(Object.keys(response.data.rates));
+    }).catch((error) => {
+      console.log(error);
+    });
+    
+  }, []);
+
+  const convertCurrency = () => {
+    axios.get(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`).then((response) => {
+      const rate = response.data.rates[toCurrency];
+      setConvertedAmount((amount * rate).toFixed(2));
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline></CssBaseline>
+        <Container maxWidth="sm">
+          <Typography variant="h4" align="center" className='margin-1rem'>Currency Converter App</Typography>
+
+          <div className='margin-1rem'>
+            <TextField
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              type="number"
+              fullWidth
+            />
+          </div>
+
+          <div className='margin-1rem'>
+            <Select
+              value={fromCurrency}
+              onChange={(e) => {
+                setFromCurrency(e.target.value);
+                setConvertedAmount(null);
+              }}
+              fullWidth
+            >
+              {currencies.map((currency) => (
+                <MenuItem key={currency} value={currency}>{currency}</MenuItem>
+              ))}
+            </Select>
+          </div>
+
+          <div className='margin-1rem'>
+            <Select
+              value={toCurrency}
+              onChange={(e) => {
+                setToCurrency(e.target.value);
+                setConvertedAmount(null);
+              }}
+              fullWidth
+            >
+              {currencies.map((currency) => (
+                <MenuItem key={currency} value={currency}>{currency}</MenuItem>
+              ))}
+            </Select>
+
+          </div>
+
+          <div className='margin-1rem button'>
+
+            <Button
+              onClick={convertCurrency}
+              variant="outlined"
+              // size='large'
+              fullWidth
+            >
+              <CurrencyExchangeIcon />
+              {/* Convert */}
+            </Button>
+          </div>
+          
+          {convertedAmount && (
+            <Typography variant="h5" align="center">
+              {amount} {fromCurrency} = {convertedAmount} {toCurrency}
+            </Typography>
+          )}
+
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
